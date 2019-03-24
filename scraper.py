ecec9e174a3386ebe9a0243c45a43a1dad4a9355
Jason Chan <jasonchanhku@gmail.com>
2019-03-24 21:36:15 +0800

    completed up to estate level

--- scraper.py
+++ scraper.py
@@ -1,48 +1,67 @@
-
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
+import time
+import pandas as pd
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument("--start-maximized")
 
 
-class Scraper(object):
-
-    def __init__(self):
-        self.url = "https://www.hangseng.com/en-hk/e-valuation/address-search/"
-        self.driver = webdriver.Chrome("/Users/jasonchan/PycharmProjects/hang-seng-scraping/chromedriver")
-        self.driver.set_window_size(1280, 1024)
-
-    def get_area_select(self):
-        path = '//select[@id="areaValue"]'
-        area_select_elem = self.driver.find_element_by_xpath(path)
-        area_select = Select(area_select_elem)
-
-        return area_select
-
-    def load_page(self):
-        self.driver.get(self.url)
-
-        def page_loaded(driver):
-            path = '//select[@id="areaValue"]'
-            return driver.find_element_by_xpath(path)
-
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(page_loaded)
-
-    def scrape(self):
-
-        def scrape_area():
-
-            area_select = self.get_area_select()
-            area_values = [area.get_attribute('text') for area in area_select.options[1:]]
-
-            return area_values
-
-        self.load_page()
-
-        for area in scrape_area():
-            print(area)
+url = "https://www.hangseng.com/en-hk/e-valuation/address-search/"
+driver = webdriver.Chrome("/Users/jasonchan/PycharmProjects/hang-seng-scraping/chromedriver", options=options)
+driver.set_window_size(1280, 1024)
+driver.get(url)
 
 
-if __name__ == "__main__":
-    scraper = Scraper()
-    scraper.scrape()
+path = '//span[@class = "select2-selection select2-selection--single"]'
+
+areas_li = []
+districts_li = []
+estates_li = []
+
+element = driver.find_elements_by_xpath('//span[@class="rvp_subtitle"]')
+driver.execute_script("arguments[0].scrollIntoView()", element[1])
+
+
+driver.find_elements_by_xpath(path)[0].click()
+areas = [i.text for i in driver.find_elements_by_xpath('//li[@class="select2-results__option"]')]
+
+for i in range(len(areas)):
+
+    if i is not 0:
+        driver.find_elements_by_xpath(path)[0].click()
+
+    # print(f"Area: {areas[i]}")
+    driver.find_elements_by_xpath('//li[@class="select2-results__option"]')[i].click()
+    time.sleep(3)
+    driver.find_elements_by_xpath(path)[1].click()
+    districts = [i.text for i in driver.find_elements_by_xpath('//li[@class="select2-results__option"]')]
+    #print(f"Districts under {areas[i]}: ")
+
+    for j in range(len(districts)):
+        #print(districts[j])
+
+        if j is not 0:
+            driver.find_elements_by_xpath(path)[1].click()
+
+        driver.find_elements_by_xpath('//li[@class="select2-results__option"]')[j].click()
+        time.sleep(3)
+        driver.find_elements_by_xpath(path)[2].click()
+        estates = [i.text for i in driver.find_elements_by_xpath('//li[@class="select2-results__option"]')]
+        #print(f"Estates under {districts[j]}: ")
+
+        for k in range(len(estates)):
+            #print(estates[k])
+            areas_li.append(areas[i])
+            districts_li.append(districts[j])
+            estates_li.append(estates[k])
+
+            print(areas[i] + ', ' + districts[j] + ', ' + estates[k])
+
+
+
+df = pd.DataFrame()
+df["Area"] = areas_li
+df["District"] = districts_li
+df["Estate"] = estates_li
+df.to_csv('hang-seng-data.csv', index=False)
